af81da9f5a131bc1e8d6b26b19e84bf9ba2d78b7
espilinter <FARSHAD.fsn@gmail.com>
2023-05-14 19:11:20 +0430

    make footer responsive

--- src/layouts/footer/footer.layout.jsx
+++ src/layouts/footer/footer.layout.jsx
@@ -12,8 +12,8 @@ import "./footer.layout.css"
 const Footer = () => {
     return (
         <footer className="bg-[#dcdcdc] background pb-28">
-            <div className="flex justify-between w-[1300px] m-auto pt-40">
-                <div className="w-339 h-134">
+            <div className="flex w-98% 1220:w-90% m-auto pt-40">
+                <div className="w-26% h-134">
                     <div className=" flex">
                         <div className="h-134">
                             <img src={kelidLogo} className="" />
@@ -21,36 +21,36 @@ const Footer = () => {
                         <p className="w-209 text-12 mt-14 text-right mr-16">بزرگترین سامانه کارآفرینی و ایجاد اشتغال جوانان و دانشجویان ایران در فضای مجازی و واقعی و در زمینه تبلیغات ، خرید و فروش ، خدمات ، گردشگری ، واردات و صادرات ، بازرگانی و روابط بین المللی ایران</p>
 
                     </div>
-                    <div className="flex mt-40">
+                    <div className=" flex mt-40">
                         <div className="h-134 mt-20">
                             <img src={logoBlack} />
                         </div>
                         <p className="w-209 text-12 mt-14 text-right mr-16">از نزدیک به یک قرن پیش پرچم دار خرید و فروش ملک در ایران بوده ایم. پس از گذشت 30 سال از آغاز تجارت در این حوزه درسال 1377 شمسی به پشتوانه کسب رضایت مشتریان گام در مسیر تولید انبوه صنعتی نهادیم.</p>
                     </div>
                 </div>
-                <div className="flex justify-between w-65% text-[#8A8A8A]">
+                <div className="flex justify-between w-65% text-[#8A8A8A] mr-6%">
                     <div>
-                        <h2 className="text-right font-bold text-16 mb-24 text-[#1E1E1E]">ابر پت شاپ</h2>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">درباره ما</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">مجله</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">فرصت شغلی</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">استخدام در هولدینگ</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">باشگاه مشتریان</a>
+                        <h2 className="text-right font-bold text-14 1150:text-16 mb-24 text-[#1E1E1E]">ابر پت شاپ</h2>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">درباره ما</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">مجله</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">فرصت شغلی</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">استخدام در هولدینگ</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">باشگاه مشتریان</a>
                     </div>
                     <div>
-                        <h2 className="text-right font-bold text-16 mb-24 text-[#1E1E1E]">پشتیبانی</h2>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">راهنمای درج آگهی</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">پرسش های متداول</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">انتقادات و پیشنهادات</a>
+                        <h2 className="text-right font-bold text-14 1150:text-16 mb-24 text-[#1E1E1E]">پشتیبانی</h2>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">راهنمای درج آگهی</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">پرسش های متداول</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">انتقادات و پیشنهادات</a>
                     </div>
                     <div>
-                        <h2 className="text-right font-bold text-16 mb-24 text-[#1E1E1E]">اطلاعات قانونی</h2>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">قوانین سایت</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">اشتراک</a>
-                        <a href="#" className="font-normal text-14 leading-10 block text-right">حریم خصوصی</a>
+                        <h2 className="text-right font-bold text-14 1150:text-16 mb-24 text-[#1E1E1E]">اطلاعات قانونی</h2>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">قوانین سایت</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">اشتراک</a>
+                        <a href="#" className="font-normal text-12 1150:text-14 leading-10 block text-right">حریم خصوصی</a>
                     </div>
                     <div className="w-278">
-                        <h2 className="text-right font-bold text-16 mb-24 text-[#1E1E1E]">تماس با ما</h2>
+                        <h2 className="text-right font-bold text-12 1150:text-14 1150:text-16 mb-24 text-[#1E1E1E]">تماس با ما</h2>
                         <div className="text-right flex">
                             <FiMapPin className=" inline-block w-20 mt-7 " />
                             <p className=" font-normal text-14 leading-8 inline-block w-246 text-right">تهران - يوسف آباد - خيابان فتحی شقاقی
@@ -68,8 +68,8 @@ const Footer = () => {
                 </div>
 
             </div>
-            <div className="w-[1300px] m-auto bg-[#6E6E6E] text-[#6E6E6E] h-1 block mt-114"></div>
-            <div className="h-83 w-[1300px] pt-40 flex justify-between m-auto">
+            <div className="w-90% m-auto bg-[#6E6E6E] text-[#6E6E6E] h-1 block mt-114"></div>
+            <div className="h-83 w-90% pt-40 flex justify-between m-auto">
                 <div className="h-83 w-345">
                     <h4 className="font-medium text-14 text-right">باشگاه مشتریان</h4>
                     <div className="mt-4 text-right flex">
